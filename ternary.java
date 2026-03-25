016343ecd9b0131e9cf180f66d46dfaff2dff61c
Elsie <156831523+oakgreybookshelf@users.noreply.github.com>
2026-03-25 15:31:36 -0400

    Create ternary.java

---
+++ ternary.java
@@ -0,0 +1,9 @@
+// ternary operator ex:
+
+class GFG {
+    public static void main (String[] args) {
+        int a = 10, b = 20;
+        int res = (a > b) ? a : b; // ? is the ternary operator
+        System.out.println(res);
+    }
+}
